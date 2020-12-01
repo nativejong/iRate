fcd7e46fcab86365df58711f697c16d7ef765e6c
Wiphu Blaine <Wiphu Blaine>
2020-12-01 20:53:57 +0700

    ok

--- Example/Example/rateVC.swift
+++ Example/Example/rateVC.swift
@@ -11,20 +11,22 @@ import iRate
 
 
 class rateVC: UIViewController {
-//    var music : iRate?
+    var rate : iRate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.edgesForExtendedLayout = []
-//        music = iRate(frame: CGRect.zero)
-//        self.view.addSubview(music!)
+        rate = iRate(frame: CGRect.zero)
+        self.view.addSubview(rate!)
+        
+        self.view.backgroundColor = UIColor.white
     }
     
 
     override func viewWillLayoutSubviews() {
         let w = self.view.frame.size.width;
         let h = self.view.frame.size.height;
-//        music?.frame = CGRect(x: w * 0.2, y: h * 0.4, width: w * 0.6, height: h * 0.1)
+        rate?.frame = CGRect(x: w * 0.2, y: h * 0.4, width: w * 0.6, height: h * 0.1)
     }
 
 }
