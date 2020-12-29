e5e9956338790cd5dacf22f9fa7ab2de767053d6
Wiphu Blaine <Wiphu Blaine>
2020-12-29 20:21:57 +0700

    ok

--- Example/Example/rateVC.swift
+++ Example/Example/rateVC.swift
@@ -12,18 +12,23 @@ import iRate
 
 class rateVC: UIViewController {
     var rate : iRate?
+    var val : UIButton?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.edgesForExtendedLayout = []
         
-        rate = iRate(frame: CGRect.zero)
+        val = UIButton(frame: CGRect.zero )
+        val?.backgroundColor = UIColor.black
+        self.view.addSubview(val!)
+
+        rate = iRate(frame: CGRect.zero, bg: UIColor.black, fg: UIColor.red)
         self.view.addSubview(rate!)
         self.view.backgroundColor = UIColor.white
-        
                 
         rate?.watchAction(1.2, rating: { (rv : CGFloat) -> Bool in
             print( String( format: "value %f", rv ) )
+            self.val?.setTitle(String( format: "%.2f", rv ), for: UIControl.State.normal)
             return( true )
         })
     }
@@ -32,7 +37,8 @@ class rateVC: UIViewController {
     override func viewWillLayoutSubviews() {
         let w = self.view.frame.size.width;
         let h = self.view.frame.size.height;
-        rate?.frame = CGRect(x: w * 0.2, y: h * 0.4, width: w * 0.6, height: h * 0.1)
+        rate?.frame = CGRect(x: w * 0.2, y: h * 0.3, width: w * 0.6, height: h * 0.1)
+        val?.frame = CGRect(x: w * 0.2, y: h * 0.45, width: w * 0.6, height: h * 0.1)
     }
 
 }
